e75415579084638e73b2e95dfd93fd93ba7533f8
Gabriel Costa <gabcostta37@gmail.com>
2024-08-06 15:41:27 -0300

    Finalizando component ProductViewPage: - Falta ajustar meus pedidos, categorias e formulario para page de login.

--- src/ui/components/Header/Header.jsx
+++ src/ui/components/Header/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
               <li><Link to="/" className="nav-footer-home">Home</Link></li>
               <li><Link to="/ProductList" className="nav-footer-produtos">Produtos</Link></li>
               <li><Link to="/Error" className="nav-footer-categorias">Categorias</Link></li>
-              <li><Link to="/Pedidos" className="nav-footer-pedidos">Meus Pedidos</Link></li>
+              <li><Link to="/Error" className="nav-footer-pedidos">Meus Pedidos</Link></li>
             </div>
           </div>
         </nav>
